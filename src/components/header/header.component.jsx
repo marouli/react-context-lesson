aefeea1555ff4d1e24d3c169d7503f9ba2869366
Maria Sidiropoulou <maroulisid@gmail.com>
2021-01-18 16:46:32 +0100

    cart context provider

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,18 +4,16 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-
 import CurrentUserContext from '../../contexts/current-user/current-user.context';
-import CartContext from '../../contexts/cart/cart.context';
+import { CartContext } from '../../providers/cart/cart.provider';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
- 
+
 import './header.styles.scss';
 
 const Header = () => {
   const currentUser = useContext(CurrentUserContext);
-  const [hidden, setHidden] = useState(true);
-  const  toggleHidden = () => setHidden(!hidden);
+  const { hidden } = useContext(CartContext);
 
   return (
     <div className='header'>
@@ -38,18 +36,11 @@ const Header = () => {
             SIGN IN
           </Link>
         )}
-        <CartContext.Provider 
-          value={{ 
-            hidden, 
-            toggleHidden 
-          }}
-        >
-          <CartIcon />
-        </CartContext.Provider>
+        <CartIcon />
       </div>
       {hidden ? null : <CartDropdown />}
     </div>
-  )
+  );
 };
 
 export default Header;
